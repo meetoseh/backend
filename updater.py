34831a3e6b3cd1e7dc8dff01fd2b37e4e1d8b572
Timothy Moore <mtimothy984@gmail.com>
2023-01-11 09:08:36 -0800

    debug updater

--- updater.py
+++ updater.py
@@ -9,10 +9,15 @@ async def _listen_forever():
     """Subscribes to the redis channel updates:backend and upon
     recieving a message, calls /home/ec2-user/update_webapp.sh
     """
+    print("updater subscribing")
     async with pps.PPSSubscription(pps.instance, "updates:backend", "updater") as sub:
+        print("updater waiting for message")
         await sub.read()
+        print("updater received message")
 
+    print("updater triggering update_webapp.sh")
     if platform.platform().lower().startswith("linux"):
+        print("using linux strategy")
         subprocess.Popen(
             "bash /home/ec2-user/update_webapp.sh > /dev/null 2>&1",
             shell=True,
@@ -22,6 +27,7 @@ async def _listen_forever():
             preexec_fn=os.setpgrp,
         )
     else:
+        print("using windows strategy")
         subprocess.Popen(
             "bash /home/ec2-user/update_webapp.sh",
             creationflags=subprocess.CREATE_NEW_PROCESS_GROUP,

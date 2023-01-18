6a478b285d83300dde3f702417804a4095b5a18b
Timothy Moore <mtimothy984@gmail.com>
2023-01-18 10:34:18 -0800

    Add backend ready message

--- updater.py
+++ updater.py
@@ -5,6 +5,7 @@ import asyncio
 import subprocess
 import platform
 import secrets
+import socket
 import os
 
 
@@ -14,6 +15,9 @@ async def _listen_forever():
     """
     async with Itgs() as itgs:
         await release_update_lock_if_held(itgs)
+
+        slack = await itgs.slack()
+        await slack.send_ops_message(f"backend {socket.gethostname()} ready")
 
     async with pps.PPSSubscription(pps.instance, "updates:backend", "updater") as sub:
         await sub.read()

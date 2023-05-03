cd12a0482f42d463b1e4f2131a44ce821a0f9db1
Timothy Moore <mtimothy984@gmail.com>
2023-05-03 11:28:30 -0700

    py3.8 support

--- emotions/lib/emotion_content.py
+++ emotions/lib/emotion_content.py
@@ -1,4 +1,4 @@
-from typing import Callable, Coroutine, List, Never, Optional
+from typing import Callable, Coroutine, List, NoReturn as Never, Optional
 from error_middleware import handle_error
 from itgs import Itgs
 from emotions.routes.read import Emotion

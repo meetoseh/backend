6a473ad8aac293fac9cec21a3c5dc302885d4d71
Timothy Moore <mtimothy984@gmail.com>
2024-02-12 08:50:20 -0800

    Include progress for journey background images

--- journeys/background_images/routes/create.py
+++ journeys/background_images/routes/create.py
@@ -1,9 +1,10 @@
+import secrets
 from typing import Optional
 from fastapi import APIRouter, Header
 from fastapi.responses import Response
 from pydantic import BaseModel, Field
 from auth import auth_admin
-from file_uploads.helper import FileUploadResponse, start_upload
+from file_uploads.helper import FileUploadWithProgressResponse, start_upload
 from models import STANDARD_ERRORS_BY_CODE
 from itgs import Itgs
 
@@ -16,7 +17,7 @@ class CreateJourneyBackgroundImageRequest(BaseModel):
 
 @router.post(
     "/",
-    response_model=FileUploadResponse,
+    response_model=FileUploadWithProgressResponse,
     responses=STANDARD_ERRORS_BY_CODE,
     status_code=201,
 )
@@ -37,16 +38,21 @@ async def create_journey_background_image(
         if auth_result.result is None:
             return auth_result.error_response
 
+        job_progress_uid = f"oseh_jp_{secrets.token_urlsafe(16)}"
         res = await start_upload(
             itgs,
             file_size=args.file_size,
             success_job_name="runners.process_journey_background_image",
-            success_job_kwargs={"uploaded_by_user_sub": auth_result.result.sub},
+            success_job_kwargs={
+                "uploaded_by_user_sub": auth_result.result.sub,
+                "job_progress_uid": job_progress_uid,
+            },
             failure_job_name="runners.delete_file_upload",
             failure_job_kwargs=dict(),
+            job_progress_uid=job_progress_uid,
         )
         return Response(
-            content=res.model_dump_json(),
+            content=res.__pydantic_serializer__.to_json(res),
             headers={"Content-Type": "application/json; charset=utf-8"},
             status_code=201,
         )

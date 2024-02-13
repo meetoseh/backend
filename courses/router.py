da3b04c9ac435e7704cdb1a776c052ee40690fb2
Timothy Moore <mtimothy984@gmail.com>
2024-02-13 07:45:31 -0800

    Delete unused /courses/mine

--- courses/router.py
+++ courses/router.py
@@ -2,7 +2,6 @@ from fastapi import APIRouter
 import courses.routes.activate
 import courses.routes.attach_free
 import courses.routes.attach
-import courses.routes.mine
 import courses.routes.start_next
 import courses.routes.advance
 
@@ -16,7 +15,6 @@ router = APIRouter()
 router.include_router(courses.routes.activate.router)
 router.include_router(courses.routes.attach_free.router)
 router.include_router(courses.routes.attach.router)
-router.include_router(courses.routes.mine.router)
 router.include_router(courses.routes.start_next.router)
 router.include_router(courses.routes.advance.router)
 

05e3adc6c79152c90898223737db06bc74ba8ee7
Timothy Moore <mtimothy984@gmail.com>
2024-04-02 09:49:02 -0700

    Spawn more backend workers

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,7 +2,7 @@
 main() {
     . venv/bin/activate
     . /home/ec2-user/config.sh
-    uvicorn main:app --port 80 --host 0.0.0.0
+    uvicorn main:app --workers 16 --port 80 --host 0.0.0.0
 }
 
 main

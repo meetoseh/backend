48d81b7f672140a6f7978bdea4f19d6dd119450d
Timothy Moore <mtimothy984@gmail.com>
2024-04-02 11:07:22 -0700

    Use 2 workers per instance, update pydantic
    using 465mb/process right now which is high, but tracing shows no
    obvious issues - it's all storing the actual functions/docs (which is
    just very expensive in python) + the models

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,7 +3,7 @@ main() {
     . venv/bin/activate
     . /home/ec2-user/config.sh
     SET AWS_METADATA_SERVICE_NUM_ATTEMPTS=5
-    uvicorn main:app --port 80 --host 0.0.0.0
+    uvicorn main:app --workers 2 --port 80 --host 0.0.0.0
 }
 
 main

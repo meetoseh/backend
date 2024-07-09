277140b53020935055d8a1faa6cee1d31d209bb8
Timothy Moore <mtimothy984@gmail.com>
2024-07-09 08:17:35 -0700

    Fix 0202 migration

--- migrations/runners/0202_winds_of_change_fix.py
+++ migrations/runners/0202_winds_of_change_fix.py
@@ -13,7 +13,7 @@ async def up(itgs: Itgs):
 
     await cursor.execute(
         "UPDATE journeys SET variation_of_journey_id = NULL WHERE uid = ?",
-        "oseh_j_yE4F2aUAkiGXXiryzvUUUQ",
+        ["oseh_j_yE4F2aUAkiGXXiryzvUUUQ"],
     )
     await evict_external_journey(itgs, uid="oseh_j_yE4F2aUAkiGXXiryzvUUUQ")
     await evict_external_journey(itgs, uid="oseh_j_f6b2hsoJ61HPrH3MsrGhgQ")
